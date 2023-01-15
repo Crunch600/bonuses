1449ec6fa27968c383d29acba37ec2a3ce55497c
Dmitrii Yablochkin <yablochkind89@gmail.com>
2023-01-15 19:42:15 +0300

    Change one

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,16 @@ public class Main {
     public static void main(String[] args) {
 
         int invoice = 100;
-        int amountPayment = 100000;
+        int amountPayment = 1000;
 
-        if (amountPayment >= 1000) {
+        if (amountPayment > 1000) {
             System.out.println("Бонусы начислены");
         } else {
             System.out.println("Бонусы не начислены");
         }
 
         int amountBonus = (amountPayment / 100);
-        if (amountPayment < 1000) {
+        if (amountPayment <= 1000) {
             amountBonus = 0;
         }
 
