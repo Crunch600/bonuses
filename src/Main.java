c161bc7dfc985affe8a069cde5899462a0d376e9
Dmitrii Yablochkin <yablochkind89@gmail.com>
2023-01-15 18:05:40 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int invoice = 100;
+        int amountPayment = 100000;
+
+        if (amountPayment >= 1000) {
+            System.out.println("Бонусы начислены");
+        } else {
+            System.out.println("Бонусы не начислены");
+        }
+
+        int amountBonus = (amountPayment / 100);
+        if (amountPayment < 1000) {
+            amountBonus = 0;
+        }
+
+        int finalInvoice = (invoice + amountPayment + amountBonus);
+
+        System.out.println("Количество бонусов: " + amountBonus + " руб.");
+        System.out.println("Итоговый счет: " + finalInvoice + " руб.");
+
+    }
+}
